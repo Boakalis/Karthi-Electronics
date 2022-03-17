449c5dc0754946ae82e9a0445220184a7ba40943
BoopathyDrag <Boopathy.kr@codetez.com>
2022-03-17 11:09:06 +0530

    Removed Redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,10 +38,10 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('guest')->except('logout');
+    // }
 
     public function showLoginForm(){
         return view('admin.login');
