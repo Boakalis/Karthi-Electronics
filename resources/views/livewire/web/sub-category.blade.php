1e0219dc5ff384fd422f07232fd6e31c4afed1d0
BoopathyDrag <Boopathy.kr@codetez.com>
2022-05-08 23:00:26 +0530

    Update sub-category.blade.php

--- resources/views/livewire/web/sub-category.blade.php
+++ resources/views/livewire/web/sub-category.blade.php
@@ -113,7 +113,7 @@
                                 </div>
                             </div>
                             <!-- Ec Pagination Start -->
-                            <div class="ec-pro-pagination">
+                            {{-- <div class="ec-pro-pagination">
                                 <span>Showing 1-12 of 21 item(s)</span>
                                 <ul class="ec-pro-pagination-inner">
                                     <li><a class="active" href="#">1</a></li>
@@ -123,7 +123,7 @@
                                     <li><a href="#">5</a></li>
                                     <li><a class="next" href="#">Next <i class="ecicon eci-angle-right"></i></a></li>
                                 </ul>
-                            </div>
+                            </div> --}}
                             <!-- Ec Pagination End -->
                         </div>
                         <!--Shop content End -->
