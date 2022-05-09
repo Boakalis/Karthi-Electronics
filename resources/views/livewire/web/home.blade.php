5f376e3cc80266f8ddfa11e14ea9b8d91b5d0f6d
BoopathyDrag <Boopathy.kr@codetez.com>
2022-05-09 14:30:06 +0530

    Update home.blade.php

--- resources/views/livewire/web/home.blade.php
+++ resources/views/livewire/web/home.blade.php
@@ -90,7 +90,7 @@
                                         <center>
                                             <h6 class="ec-pro-title fw-bold"><a
                                                     href="{{ route('web.product.detail', ['category' => $featured->subcategory->category->slug, 'product' => $featured->subcategory->slug, 'slug' => $featured->slug]) }}">
-                                                    {{ Str::limit($featured->name, 30 , '...') }}</a></h6>
+                                                    {{ Str::limit($featured->name, 12 , '...') }}</a></h6>
                                         </center>
                                         <center>
                                             <span class="ec-price">
@@ -464,7 +464,7 @@
                                             <div class="col-12">
                                                 <h6 class="ec-pro-title fw-bold"><a
                                                         href="{{ route('web.product.detail', ['category' => $product->subcategory->category->slug, 'product' => $product->subcategory->slug, 'slug' => $product->slug]) }}">
-                                                        {{ Str::limit($product->name, 30, '...') }}</a></h6>
+                                                        {{ Str::limit($product->name, 12, '...') }}</a></h6>
                                             </div>
                                         </center>
                                         <center>
