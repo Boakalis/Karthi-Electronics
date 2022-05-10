bb2564bca46c694008f92d53095e7d8355fd2bdb
BoopathyDrag <Boopathy.kr@codetez.com>
2022-05-10 10:34:05 +0530

    Update AdminController.php

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -32,7 +32,7 @@ class AdminController extends Controller
 
     public function settingsUpdate(Request $request)
     {
-        return $request;
+        
 
         $validatedData = $request->validate(
             [
